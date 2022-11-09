321e6d2dee919e2f83037159b8959fca87ff6274
ChemsCode <saidiceb@gmail.com>
2022-11-09 15:13:11 -0500

    modified allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
+    'https://soen-341-project.netlify.app',
     'http://localhost:3500',
-    'http://localhost:3000'
 ];
 
 module.exports = allowedOrigins;
