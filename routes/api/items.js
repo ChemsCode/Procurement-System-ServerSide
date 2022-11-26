269e9b9571d153913a16e7b0a8dc644e67828a17
ChemsCode <saidiceb@gmail.com>
2022-11-25 20:20:00 -0500

    fixed item api

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const itemRequestsController = require('../../controllers/itemController');
+const itemController= require('../../controllers/itemController');
 const ROLES_LIST = require('../../config/roles_list');
 const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
-    .get(itemRequestsController.getAllItems)
-    .post(verifyRoles(ROLES_LIST.Supplier), itemRequestsController.createNewItem)
-    .put(verifyRoles(ROLES_LIST.Supplier), itemRequestsController.updateItem)
-    .delete(verifyRoles(ROLES_LIST.Supplier), itemRequestsController.deleteItem);
+    .get(itemController.getAllItems)
+    .post(verifyRoles(ROLES_LIST.Supplier), itemController.createNewItem)
+    .put(verifyRoles(ROLES_LIST.Supplier), itemController.updateItem)
+    .delete(verifyRoles(ROLES_LIST.Supplier), itemController.deleteItem);
 
 router.route('/:id')
-    .get(itemRequestsController.getItem);
+    .get(itemController.getItem);
 
 module.exports = router;
